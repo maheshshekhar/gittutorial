86da0bfcac15a664429547bc155665b655e8d5f4
Mahesh Shekhar <maheshshekhar@gmail.com>
2017-08-20 18:42:37 +0530

    story 182: Fix benchmarks for speed

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 main tf file
 second line of main.tf file
 third line of main.tf
+fourth line in main.tf
--- test.tf
+++ test.tf
@@ -1,2 +1,3 @@
 this is test file
 this is the second line in the test file
+this is the third line in the test file
